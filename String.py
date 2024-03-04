4242e1135374a75eedcb5f314c6704abdb1534b9
Manzoor Ahmed <160848061+MazoorAhmed123@users.noreply.github.com>
2024-03-04 09:38:55 -0800

    Add files via upload
    string

---
+++ String.py
@@ -0,0 +1,31 @@
+"""fruit = 'Banana'
+letter = fruit[1]
+print(letter)
+print(len(fruit))
+x = 3
+w = fruit[x-1]
+print(w)"""
+
+""""fruit = 'Banana'
+index = 0
+while index < len(fruit):
+    letter = fruit[index]
+    index = index + 1
+    print(index,letter)"""
+"""a = 'Hello'
+b = a + "Hello"
+print(b)"""
+"""fruit = 'banana'
+
+if 'n' in fruit:
+    print("true")
+else:
+    print("false")"""
+fruit = 'banana'
+
+if 'c' in fruit or 'd' in fruit or 'f' in fruit or 'a' in fruit:
+    print("found it")
+else:
+    print("not found")
+
+
